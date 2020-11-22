f9df85c03903a03a4e4b1567caa9dc3284bcb14d
cxrlos <A01025948@itesm.mx>
2020-11-22 14:53:15 -0600

    Updates on table functions

--- html/index.js
+++ html/index.js
@@ -3,25 +3,10 @@ firebase.auth().onAuthStateChanged(function(user) {
         document.getElementById("user_div").style.display = "block";
         document.getElementById("login_div").style.display = "none";
 
-        var database = firebase.database();
-        database.ref('classrooms').once('value', function(snapshot){
-            if(snapshot.exists()){
-                var content = '';
-                snapshot.forEach(function(data){
-                    var val = data.val();
-                    content +='<tr>';
-                    content += '<td>' + val.classroom_code + '</td>';
-                    content += '<td>' + val.quota + '</td>';
-                    content += '<td>' + val.building + '</td>';
-                    content += '<td>' + val.floor + '</td>';
-                    content += '</tr>';
-                });
-                $('#ex-table').append(content);
-            }
-        });
     } else {
         document.getElementById("user_div").style.display = "none";
         document.getElementById("login_div").style.display = "block";
+        showClassroomTable();
    }
 
     var user = firebase.auth().currentUser;
@@ -50,5 +35,25 @@ function logout() {
         var errorCode = error.code;
         var errorMessage = error.message;
         window.alert("ERROR [" + errorCode + "]: " + errorMessage);
+    });
+}
+
+function showClassroomTable(){
+    // $('#other-table').empty();
+    var database = firebase.database();
+    database.ref('classrooms').once('value', function(snapshot){
+        if(snapshot.exists()){
+            var content = '';
+            snapshot.forEach(function(data){
+                var val = data.val();
+                content +='<tr>';
+                content += '<td>' + val.classroom_code + '</td>';
+                content += '<td>' + val.quota + '</td>';
+                content += '<td>' + val.building + '</td>';
+                content += '<td>' + val.floor + '</td>';
+                content += '</tr>';
+            });
+            $('#ex-table').append(content);
+        }
     });
 }
