80e8095734eacbddeb17709c8c6e62bdd30cc52c
cxrlos <A01025948@itesm.mx>
2020-11-22 14:35:29 -0600

    Added dynamic table example

--- html/index.js
+++ html/index.js
@@ -2,15 +2,31 @@ firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         document.getElementById("user_div").style.display = "block";
         document.getElementById("login_div").style.display = "none";
+
+        var database = firebase.database();
+        database.ref('classrooms').once('value', function(snapshot){
+            if(snapshot.exists()){
+                var content = '';
+                snapshot.forEach(function(data){
+                    var val = data.val();
+                    content +='<tr>';
+                    content += '<td>' + val.classroom_code + '</td>';
+                    content += '<td>' + val.quota + '</td>';
+                    content += '<td>' + val.building + '</td>';
+                    content += '<td>' + val.floor + '</td>';
+                    content += '</tr>';
+                });
+                $('#ex-table').append(content);
+            }
+        });
     } else {
         document.getElementById("user_div").style.display = "none";
         document.getElementById("login_div").style.display = "block";
-    }
+   }
 
     var user = firebase.auth().currentUser;
     if(user != null){
         var student_id = user.email.substr(0, user.email.indexOf('@'));
-        console.log(student_id);
         document.getElementById("user_text").innerHTML = "Welcome to the system " + student_id;
     }
 
